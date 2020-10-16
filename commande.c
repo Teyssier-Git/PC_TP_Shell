bc1f6d7d2ca6fe505cb81526c722ae0be33f1527
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-10-16 16:55:39 +0200

    Code cd, waiting for theo

--- commande.c
+++ commande.c
@@ -45,27 +45,34 @@ int set (char* name, char**envp) {
 */
 
 int cd (char **envp,char *name) {
-  DIR* dir = opendir(name);
-  if (dir) {
-      int i = find_var_env(envp, "PWD");
-      char **ligne = separate(envp[i], "=");
-      char *new_pwd = (char *) malloc(sizeof(char) * (strlen(ligne[1]) + 1 + strlen(name));
-      int act;
-      for (act = 0; ligne[1][act] != '\0'; i++)
-        new_pwd[act] = ligne[1][act];
+  char **path = separate(name, '/');
+  for (int act_dir = 0; path[act_dir] != NULL; act_dir ++) {
+    if (path[act_dir] == "..") {
 
-      new_pwd[act++] = '/';
-      act ++;
-      for (int act_n = 0; name[act_n] != '\0'; act_n ++)
-        new_pwd[act + act_n] = name[act_n];
+    } else if (path[act_dir] == "~") {
+      int indice_pwd = find_var_env(envp, "PWD");
+      char *old _pwd= separate(envp[indice_pwd], "=")[1];
+    } else {
+      DIR* dir = opendir(path[act_dir]);
+      if (dir) {
+          int indice_pwd = find_var_env(envp, "PWD");
+          char *old _pwd= separate(envp[indice_pwd], "=")[1];
+          char *new_pwd = (char *) malloc(sizeof(char) * (strlen(old_pwd) + 1 + strlen(path[act_dir]));
+          int size_old_pwd = strlen(old_pwd);
+          new_pwd = strncpy (new_pwd,old_pwd, size_old_pwd);
+          new_pwd = strncpy (new_pwd + size_old_pwd ,"/", 1);
+          new_pwd = strncpy (new_pwd + size_old_pwd + 1,path[act_dir], strlen(path[act_dir]));
+          set(envp, "PWD", new_pwd);
+          closedir(dir);
+          return 0;
+      } else if (ENOENT == errno) {
+          printf("bash: cd: %s: No such file or directory\n", name);
+      } else {
+          /* opendir() failed for some other reason. */
+      }
+    }
 
-      set(envp, "PWD", new_pwd);
-      closedir(dir);
-      return 0;
-  } else if (ENOENT == errno) {
-      printf("bash: cd: %s: No such file or directory\n", name);
-  } else {
-      /* opendir() failed for some other reason. */
   }
+
   return 1;
 }
